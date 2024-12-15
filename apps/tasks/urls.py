e368e6cc324bdd3744f72a6dc1ff32f1d5db3778
Bruno R. Pianca <brunorpdev@gmail.com>
2024-12-15 02:29:17 -0300

    feat: Botão troca de status

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:pk>/detail/', views.TaskDetailView.as_view(), name='detail'),
     path('<int:pk>/update/', views.TaskUpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name='delete'),
+    path('change-status/', views.change_task_status, name='change_status'),
 ]
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,6 +1,7 @@
 
 from apps.tasks.models import Task
 from apps.tasks.forms import TaskForm
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
@@ -11,6 +12,7 @@ class TaskListView(ListView):
 
     def get_queryset(self):
         query_set = super().get_queryset()
+        query_set = query_set.exclude(status=Task.TaskStatus.DONE)
         if query_param := self.request.GET.get('q'):
             query_set = query_set.filter(title__icontains=query_param)
         return query_set
@@ -43,3 +45,22 @@ class TaskDeleteView(DeleteView):
     
     def get_success_url(self):
         return reverse('tasks:list')
+
+
+def change_task_status(request):
+    """
+    Altera o status da tarefa de acordo com parâmetro enviado do frontend.
+    """
+    if request.method == 'POST':
+        task_id = request.POST.get('task_id')
+        next_status = request.POST.get('next_status')
+        if next_status and task_id:
+            task = Task.objects.filter(id=task_id).first()
+            
+            if not task:
+                return redirect('tasks:list')
+            
+            task.status = next_status
+            task.save()
+        
+        return redirect('tasks:list')
